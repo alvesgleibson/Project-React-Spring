5092df8947203c9ad12d3d07b6a7c6fafa84e29f
Gleibson Alves <alvesgleibson@gmail.com>
2021-09-14 13:16:04 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -8,8 +8,6 @@ type ChartData = {
     labels: string[];
     series: number[];
 }
-
-
 const DonutChart = () => {
 
     const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
@@ -20,17 +18,12 @@ const DonutChart = () => {
                 const data = response.data as SaleSum[];
                 const myLabels = data.map(x => x.saleName);
                 const mySeries = data.map(x => x.sum);
-
                 setChartData({ labels: myLabels, series: mySeries });
             })
 
+
     }, [])
-
-    //const mockData = {
-    //  series: [477138, 499928, 444867, 220426, 473088],
-    //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
-
+    
     const options = {
         legend: {
             show: true
--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,10 @@
 export type SaleSum = {
-    saleName : string,
-    sum: number
+    saleName : string;
+    sum: number;
+}
+
+export type SaleSuccess = {
+    saleName: string;
+    visited: number;
+    deals: number;
 }
